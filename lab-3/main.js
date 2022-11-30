0ddd5b5f580301660b76df78af6dbe44ab8274c2
woramet025 <640112418025@bru.ac.th>
2022-11-30 11:49:35 +0700

    lab-4

--- lab-3/main.js
+++ lab-3/main.js
@@ -1,12 +1,12 @@
-var num =10;
-let text ="diw";
-//let Array =[10,15,"diw"];
-const obj = { name: "diw",age: 22 };
+var num = 10;
+let name = "Win";
+//   let Array = [10, 15, "Win"];
+const obj = { name: "win", age: 35 };
 const objMix = {
-    data:[
-        {name :"Dang",gpa:4.3,},
-        {name :"Dum",gpa:2.8,},
-    ],
+  data: [
+    { name: "Dang", gpa: 4.3 },
+    { name: "Dum", gpa: 2.8 },
+  ],
 };
 
 console.log(num);
@@ -19,33 +19,40 @@ console.log(string);
 string = objMix.data[1].name + Array[0];
 console.log(string);
 
-string = "<p>" + Array[2] +"</p>";
+string = "<p>" + Array[2] + "</p>";
 string = `<p>${Array}</p>`;
 
 console.log(string);
 
-function Add(a,b){
-return
+function Add(a, b) {
+  return a + b;
 }
 
-console.log(Add(3,5));
+console.log(Add(3, 5));
 
 document.getElementById("div1").innerHTML = name;
-$(function (){    
-// alert("Hello");
-$("#div1").html("Sitthikorn");
 
-$("#bt1").click (function () {
-$("#div1").html("I Love IT");
+$(function () {
+  // alert("Hello");
+  $("#div1").html("Drusawin");
 
-});
-$("#bt3").click(() =>{ //arrow funtion
-$(".in1").val("BRU");
-});
+  $("#bt1").bind("click", function () {
+    $("#div1").html("I love IT");
+  });
 
-$("#bt4").click(() =>{ 
-$(".in1:odd").addClass("red");
-});
+  $("#bt2").click(function () {
+    $("#div1").html("I love IT");
+  });
 
-$("#div_about").load("./pages/about.html");
-});//jQuery Handle
+  $("#bt3").click(() => {
+    //arrow function
+    $(".in1").val("BRU");
+  });
+  
+  $("#bt4").click(() => {
+    $(".in1:odd").addClass("red");
+  });
+
+  $("#div_about").load("./pages/about.html");
+  $("#div_form").load("./pages/form.html");
+}); //jQuery Handle
